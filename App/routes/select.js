5974723f4a7a2a786366c51f41d2ece2057218f2
Shannonwje <e0325213@u.nus.edu>
2020-05-05 14:46:23 +0800

    Connecting Database to webpage

--- App/routes/select.js
+++ App/routes/select.js
@@ -2,22 +2,33 @@
 var express = require('express');
 var router = express.Router();
 
-const sql_query = require('../database/allQueries');
+/*const sql_query = require('../database/allQueries');*/
 
 const { Pool } = require('pg')
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL
-})
+});
+
 /* required for database connection */
+
+var sql_query = 'SELECT * FROM customers';
+
+router.get('/', function(req, res, next) {
+	pool.query(sql_query, (err, data) => {
+		res.render('select', { title: 'Database Connect', data: data.rows });
+	});
+});
+
 
 /* SQL Query */
 /* this was the example query
 
 router.get('/', function(req, res, next) {
 	pool.query(sql_query.query.QUERY_FUNC_NAME, (err, data) => {
-		// what you want it to do
+		SELECT * FROM customers;
 	});
 });
-
 */
+
+
 module.exports = router;
